734f1bdc6c95b40002a19ab97d1a72a14cb8a358
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2023-07-31 20:13:10 +0700

    Add express server

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import jsonServer from 'json-server';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+
+const p = __dirname + '/dist/'
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(p))
+app.use('/db', jsonServer.router('./src/database/Orders.json'))
+
+app.get('/', (req, res) => {
+  res.sendFile(p + 'index.html')
+})
+
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
+})
