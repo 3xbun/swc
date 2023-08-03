b3673171249cd8cb20765d8ef419923dd3ac0e9f
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2023-08-03 16:25:32 +0700

    Fix backend rendering

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
   res.sendFile(p + 'index.html')
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(p + 'index.html')
+})
+
 app.listen(PORT, () => {
   console.log(`App running at http://localhost:${PORT}`);
 })
